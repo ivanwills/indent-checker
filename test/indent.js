11c10401abe5da03064d0c60bd142312e1772cbe
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-09-06 16:31:49 +1000

    Starting tests

---
+++ test/indent.js
@@ -0,0 +1,19 @@
+/* global require, describe, it */
+
+var assert = require('assert')
+	indent = require('index');
+
+describe('Good lines of text', function () {
+
+	it('All spaces', function () {
+		assert.equal(indent.textOk('  my line', true));
+		assert.equal(indent.textOk(' my line', true));
+		assert.equal(indent.textOk(' my line\n  another line\n', true));
+	});
+
+	it('All tabs', function () {
+		assert.equal(indent.textOk('\t\tmy line', true));
+		assert.equal(indent.textOk('\tmy line', true));
+		assert.equal(indent.textOk('\tmy line\n\t\tanother line\n', true));
+	});
+});
