b7971b39a8a9e3d581887b78c4e6caf8d94f5eb5
Ivan Wills <ivan.wills@gmail.com>
2016-09-07 20:17:39 +1000

    Fixed bracket being in the wrong place

--- test/indent.js
+++ test/indent.js
@@ -5,31 +5,31 @@ var assert = require('assert')
 
 describe('Good lines of text', function () {
 	it('All spaces', function () {
-		assert.equal(indent.textOk('my line', true));
-		assert.equal(indent.textOk('  my line', true));
-		assert.equal(indent.textOk(' my line', true));
-		assert.equal(indent.textOk(' my line\n  another line\n', true));
+		assert.equal(indent.textOk('my line'), true);
+		assert.equal(indent.textOk('  my line'), true);
+		assert.equal(indent.textOk(' my line'), true);
+		assert.equal(indent.textOk(' my line\n  another line\n'), true);
 	});
 
 	it('All tabs', function () {
-		assert.equal(indent.textOk('my line', true));
-		assert.equal(indent.textOk('\t\tmy line', true));
-		assert.equal(indent.textOk('\tmy line', true));
-		assert.equal(indent.textOk('\tmy line\n\t\tanother line\n', true));
-		assert.equal(indent.textOk('\tmy\tline', true));
+		assert.equal(indent.textOk('my line'), true);
+		assert.equal(indent.textOk('\t\tmy line'), true);
+		assert.equal(indent.textOk('\tmy line'), true);
+		assert.equal(indent.textOk('\tmy line\n\t\tanother line\n'), true);
+		assert.equal(indent.textOk('\tmy\tline'), true);
 	});
 
 	it('Files', function () {
-		assert.equal(indent.fileOk('index.js', true));
+		assert.equal(indent.fileOk('index.js'), true);
 	});
 
 });
 
 describe('Mixed lines of text', function () {
 	it('Miing tabs and spaces', function () {
-		assert.equal(indent.textOk(' \tmy line', false));
-		assert.equal(indent.textOk('\t my line', false));
-		assert.equal(indent.textOk(' \t \tmy line', false));
-		assert.equal(indent.textOk(' my line\n \t another line\n', false));
+		assert.equal(indent.textOk(' \tmy line'), false);
+		assert.equal(indent.textOk('\t my line'), false);
+		assert.equal(indent.textOk(' \t \tmy line'), false);
+		assert.equal(indent.textOk(' my line\n \t another line\n'), false);
 	});
 });
