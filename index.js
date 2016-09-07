357a28970e9f154bf55a4546ed50ebf0c53648a6
Ivan Wills <ivan.wills@gmail.com>
2016-09-07 20:20:30 +1000

    Changed to assert style and making error messages better

--- index.js
+++ index.js
@@ -3,21 +3,48 @@
 var _  = require('lodash'),
 	fs = require('fs');
 
-function textOk (text, config) {
-	_.each(text.split(/\n/), function (line) {
-		try {
-			if (line.match(/^\t+ |^ +\t/)) {
-				throw "Mixed tabs and spaces on indent!";
+function assertIndent (text, config) {
+	if (!config) {
+		config = {};
+	}
+	var indents = config.type || false;
+
+	_.each(text.split(/\n/), function (line, lineNo) {
+		if (indents === 'tabs') {
+			if (line.match(/^ /)) {
+				throw 'Using spaces in a file indented with tabs, starting line ' + lineNo + '!';
 			}
-		} catch (e) {
-			return false;
+		}
+		else if (indents === 'spaces') {
+			if (line.match(/^\t/)) {
+				throw 'Using tabs in a file indented with spaces, starting line ' + lineNo + '!';
+			}
+		}
+		else if (line.match(/^\s+/)) {
+			// set the default indent style for the text when we first see
+			// an indented line
+			indents = line.match(/^\t/) ? 'tabs' : 'spaces';
 		}
 
-		return true;
+		// always check for mixed indentation
+		if (line.match(/^\t+ |^ +\t/)) {
+			throw 'Mixed tabs and spaces for indentation on line ' + lineNo + '!';
+		}
 	});
+}
+
+function textOk (text, config) {
+	try {
+		assertIndent(text, config);
+	} catch (e) {
+		return false;
+	}
+
+	return true;
 };
 
 module.exports = {
+	ssertIndent: assertIndent,
 	textOk: textOk,
 	fileOk: function (file, config) {
 		return textOk(fs.readFileSync(file, 'utf-8'), config);
