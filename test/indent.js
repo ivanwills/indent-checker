b56380a12425e8928ba970ffa431ca53a231acc3
Ivan Wills <ivan.wills@gmail.com>
2016-09-07 20:49:17 +1000

    Added arrow functions

--- test/indent.js
+++ test/indent.js
@@ -3,15 +3,15 @@
 var assert = require('assert')
 	indent = require('../index');
 
-describe('Good lines of text', function () {
-	it('All spaces', function () {
+describe('Good lines of text', () => {
+	it('All spaces', () => {
 		assert.equal(indent.textOk('my line'), true);
 		assert.equal(indent.textOk('  my line'), true);
 		assert.equal(indent.textOk(' my line'), true);
 		assert.equal(indent.textOk(' my line\n  another line\n'), true);
 	});
 
-	it('All tabs', function () {
+	it('All tabs', () => {
 		assert.equal(indent.textOk('my line'), true);
 		assert.equal(indent.textOk('\t\tmy line'), true);
 		assert.equal(indent.textOk('\tmy line'), true);
@@ -19,14 +19,12 @@ describe('Good lines of text', function () {
 		assert.equal(indent.textOk('\tmy\tline'), true);
 	});
 
-	it('Files', function () {
-		assert.equal(indent.fileOk('index.js'), true);
-	});
+	it('Files', () => assert.equal(indent.fileOk('index.js'), true));
 
 });
 
-describe('Mixed lines of text', function () {
-	it('Miing tabs and spaces', function () {
+describe('Mixed lines of text', () => {
+	it('Miing tabs and spaces', () => {
 		assert.equal(indent.textOk(' \tmy line'), false);
 		assert.equal(indent.textOk('\t my line'), false);
 		assert.equal(indent.textOk(' \t \tmy line'), false);
@@ -34,64 +32,44 @@ describe('Mixed lines of text', function () {
 	});
 });
 
-describe('Asserting text status', function () {
-	it('Good text', function () {
-		assert.doesNotThrow(function () {
-			indent.assertIndent('A line\n space sepparated\n');
-		}, 'No error thrown for good text with space indents');
-		assert.doesNotThrow(function () {
-			indent.assertIndent('  A line\n space sepparated\n');
-		}, 'No error thrown for good text with space indents');
-		assert.doesNotThrow(function () {
-			indent.assertIndent('A line\n\ttab sepparated\n');
-		}, 'No error thrown for good text with tab indents');
-		assert.doesNotThrow(function () {
-			indent.assertIndent('\t\tA line\n\ttab sepparated\n');
-		}, 'No error thrown for good text with tab indents');
+describe('Asserting text status', () => {
+	it('Good text', () => {
+		assert.doesNotThrow(() => indent.assertIndent('A line\n space sepparated\n'), 'No error thrown for good text with space indents');
+		assert.doesNotThrow(() => indent.assertIndent('  A line\n space sepparated\n'), 'No error thrown for good text with space indents');
+		assert.doesNotThrow(() => indent.assertIndent('A line\n\ttab sepparated\n'), 'No error thrown for good text with tab indents');
+		assert.doesNotThrow(() => indent.assertIndent('\t\tA line\n\ttab sepparated\n'), 'No error thrown for good text with tab indents');
 	});
 
-	it('Bad text', function () {
+	it('Bad text', () => {
 		assert.throws(
-			function () {
-				indent.assertIndent('\tA line\n space sepparated\n');
-			},
+			() => indent.assertIndent('\tA line\n space sepparated\n'),
 			/^Using spaces/,
 			'Error thrown for initally tab indented text'
 		);
 		assert.throws(
-			function () {
-				indent.assertIndent('\tA line\n \tspace sepparated\n');
-			},
+			() => indent.assertIndent('\tA line\n \tspace sepparated\n'),
 			/^Using spaces/,
 			'Error thrown for initally tab indented text'
 		);
 		assert.throws(
-			function () {
-				indent.assertIndent(' A line\n\ttab sepparated\n');
-			},
+			() => indent.assertIndent(' A line\n\ttab sepparated\n'),
 			/^Using tabs/,
 			'Error thrown for initally tab indented text'
 		);
 		assert.throws(
-			function () {
-				indent.assertIndent(' A line\n\ttab sepparated\n');
-			},
+			() => indent.assertIndent(' A line\n\ttab sepparated\n'),
 			/^Using tabs/,
 			'Error thrown for initally tab indented text'
 		);
 
 		assert.throws(
-			function () {
-				indent.assertIndent(' A line\n', {type: 'tabs'});
-			},
+			() => indent.assertIndent(' A line\n', {type: 'tabs'}),
 			/^Using spaces/,
 			'Error thrown for explicitely tab indented text'
 		);
 
 		assert.throws(
-			function () {
-				indent.assertIndent('\tA line\n', {type: 'spaces'});
-			},
+			() => indent.assertIndent('\tA line\n', {type: 'spaces'}),
 			/^Using tabs/,
 			'Error thrown for explicitely space indented text'
 		);
